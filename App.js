ae97872fe6559e86133bde367de758b3967d6361
Gelmes <marco.firsteye@gmail.com>
2021-10-13 16:39:55 -0700

    DeviceScreen moved to its own source file

--- App.js
+++ App.js
@@ -44,7 +44,8 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { styles } from './Style.js';
-import { BleManager } from 'react-native-ble-plx';
+
+import DevicesScreen from './deviceScreen.js'
 
 export const Logo = () => {
   return (
@@ -122,90 +123,6 @@ function SkateGyroScreen({navigation}) {
       <TitleImage />
       <BottomHome onClick={handleClick} />
     </Box>
-  );
-}
-
-function DeviceList({devices}){
-  const deviceList = Object.keys(devices).reduce((array, key) => {
-    return [...array, {id: devices[key].id, name:devices[key].name}]
-  }, [])
-
-  const map = item => (
-    <Button borderWidth={1} borderColor="gray.100" bgColor="white">
-      <Text>{item.name}</Text>
-      <Text>{item.id}</Text>
-    </Button> 
-  )
-
-  return (
-    <FlatList data={deviceList} renderItem={ ({item}) => (
-      map(item)
-    )} />
-  )
-}
-
-function DevicesScreen({route, navigation}) {
-  const [count, setCount] = useState(0);
-  const [devices, setDevices] = useState({});
-  const [bleManager, setBleManager] = useState(null);
-  const [isScanning, setIsScanning] = useState(false);
-
-  async function startScan(){
-    if(bleManager == null){
-      setBleManager(new BleManager()); 
-      return
-    }
-    setIsScanning(true)
-    console.log("Start Scan Effect starting scan")
-    let tempDevices = {}
-    setTimeout(async () => {
-      setIsScanning(false)
-      console.log("Stop scan 2")
-      await bleManager.stopDeviceScan()
-      setDevices({...devices, ...tempDevices});
-    }, 5000)
-
-    bleManager.startDeviceScan([], {}, (error, device) => {
-      if(error){
-        console.log(error)
-      }
-      setCount(count + 1)
-      tempDevices[device.id] = device;
-    })
-
-  }
-
-  async function stopScan() {
-    setIsScanning(false)
-    if(bleManager){
-      console.log("Stop scan 1")
-      await bleManager.stopDeviceScan()
-    }
-  }
-
-  useEffect (() => {
-    console.log("Manager effect")
-    if(bleManager){
-      startScan()
-    }
-  }, [bleManager])  
-
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <HStack space={3}>
-          <Spinner size="lg" animating={isScanning} />
-          <Button onPress={() => startScan()}>Scan</Button>
-          <Button onPress={() => stopScan()}>Stop</Button>
-        </HStack>
-      ),
-    })
-  }, [navigation, isScanning]);
-
-  return (
-    <SafeAreaView>
-      <DeviceList devices={devices} />
-    </SafeAreaView>
   );
 }
 
--- deviceScreen.js
+++ deviceScreen.js
@@ -1,127 +1,103 @@
 
+import React, {
+  useState,
+  useEffect
+} from 'react';
 
-function DeviceList({devices}){
-    const deviceList = Object.keys(devices).reduce((array, key) => {
-      return [...array, {id: devices[key].id, name:devices[key].name}]
-    }, [])
-    console.log("New devices", deviceList)
-  
-    const map = item => (
-      <Button borderWidth={1} borderColor="gray.300" bgColor="white">
-        <Text>{item.name}</Text>
-        <Text>{item.id}</Text>
-      </Button> 
-    )
-  
-    return (
-      <FlatList data={deviceList} renderItem={ ({item}) => (
-        map(item)
-      )} />
-    )
+import {
+  SafeAreaView
+} from 'react-native';
+
+import {
+  Text,
+  Button,
+  HStack,
+  FlatList,
+  Spinner
+} from 'native-base';
+
+import { BleManager } from 'react-native-ble-plx';
+
+export function DeviceList({devices}){
+  const deviceList = Object.keys(devices).reduce((array, key) => {
+    return [...array, {id: devices[key].id, name:devices[key].name}]
+  }, [])
+
+  const map = item => (
+    <Button borderWidth={1} borderColor="gray.100" bgColor="white">
+      <Text>{item.name}</Text>
+      <Text>{item.id}</Text>
+    </Button> 
+  )
+
+  return (
+    <FlatList data={deviceList} renderItem={ ({item}) => (
+      map(item)
+    )} />
+  )
 }
-  
+
 export default function DevicesScreen({route, navigation}) {
-    const [count, setCount] = useState(0);
-    const [devices, setDevices] = useState({});
-    const [bleManager, setBleManager] = useState(null);
-    const [bleAllowed, setBleAllowed] = useState(false);
-  
-    const startScan = () =>{
-      console.log("Start Scan Effect starting scan")
-      let tempDevices = {}
-      setTimeout(async () => {
-        console.log("Stop scan")
-        await bleManager.stopDeviceScan()
-        setDevices({...devices, ...tempDevices});
-        console.log(Object.keys(tempDevices))
-      }, 5000)
-  
-      bleManager.startDeviceScan([], {}, (error, device) => {
-        if(error){
-          console.log(error)
-        }
-        setCount(count + 1)
-        // console.log(device.id)
-        // devices[device.id] = device
-        // setDevices([...devices, device]);
-        // setDevices({...devices, [device.id]:device});
-        // console.log(Object.keys(devices))
-  
-        tempDevices[device.id] = device;
-        // console.log(Object.keys(tempDevices))
-        // console.log(devices)
-      
-      })
-  
+  const [count, setCount] = useState(0);
+  const [devices, setDevices] = useState({});
+  const [bleManager, setBleManager] = useState(null);
+  const [isScanning, setIsScanning] = useState(false);
+
+  async function startScan(){
+    if(bleManager == null){
+      setBleManager(new BleManager()); 
+      return
     }
-  
-    useLayoutEffect (() => {
-      if(bleAllowed == true){
-        console.log("Effect starting scan")
-        let tempDevices = {}
-        bleManager.startDeviceScan([], {}, (error, device) => {
-          if(error){
-            console.log(error)
-          }
-          setCount(count + 1)
-          // console.log(device.id)
-          // devices[device.id] = device
-          // setDevices({...devices, [device.id]:device});
-          // setDevices([...devices, device]);
-          tempDevices[device.id] = device;
-          console.log(Object.keys(tempDevices))
-          // console.log(devices)
-        
-        })
+    setIsScanning(true)
+    console.log("Start Scan Effect starting scan")
+    let tempDevices = {}
+    setTimeout(async () => {
+      setIsScanning(false)
+      console.log("Stop scan 2")
+      await bleManager.stopDeviceScan()
+      setDevices({...devices, ...tempDevices});
+    }, 5000)
+
+    bleManager.startDeviceScan([], {}, (error, device) => {
+      if(error){
+        console.log(error)
       }
-    }, [bleAllowed])
-  
-    const bleScanStop = async () => {
-      if(bleManager){
-        console.log("Stop scan")
-        setBleAllowed(false)
-        bleManager.stopDeviceScan()
-      }
-    }
-  
-    const bleScan = async () => {    
-      console.log("Scan")
-      if (Platform.OS === 'android' && Platform.Version >= 23) {
-          if( 
-            await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN) || 
-            await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT) || 
-            await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
-          ){
-            console.log("Permission is OK");
-            setBleManager(new BleManager())
-            startScan()
-            // setBleAllowed(true)
-          } else{
-            console.log("Permission is ERROR");
-            let result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN)
-            console.log(result)
-            result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT)
-            console.log(result)
-            result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
-            console.log(result)
-          }
-      }
-    }
-  
-    React.useLayoutEffect(() => {
-      navigation.setOptions({
-        headerRight: () => (
-          <HStack space={3}>
-            <Button onPress={bleScan}>Scan</Button>
-            <Button onPress={bleScanStop}>Stop</Button>
-          </HStack>
-        ),
-      })
-    }, [navigation]);
-  
-    return (
-      <SafeAreaView>
-        <DeviceList devices={devices} />
-      </SafeAreaView>
-    );
+      setCount(count + 1)
+      tempDevices[device.id] = device;
+    })
+
   }
+
+  async function stopScan() {
+    setIsScanning(false)
+    if(bleManager){
+      console.log("Stop scan 1")
+      await bleManager.stopDeviceScan()
+    }
+  }
+
+  useEffect (() => {
+    console.log("Manager effect")
+    if(bleManager){
+      startScan()
+    }
+  }, [bleManager])  
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HStack space={3}>
+          <Spinner size="lg" animating={isScanning} />
+          <Button onPress={() => startScan()}>Scan</Button>
+          <Button onPress={() => stopScan()}>Stop</Button>
+        </HStack>
+      ),
+    })
+  }, [navigation, isScanning]);
+
+  return (
+    <SafeAreaView>
+      <DeviceList devices={devices} />
+    </SafeAreaView>
+  );
+}
